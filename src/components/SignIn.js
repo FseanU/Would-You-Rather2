dcbc3ea9455a7a36ec13a7eda16fc49c63b4f983
Hungchen Fu <sean14xx@gmail.com>
2021-05-18 18:12:13 +0200

    refactor: Migrate to Hooks

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,73 +1,61 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { Redirect } from 'react-router-dom'
-import { setAuthedUser } from '../actions/authedUser'
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Redirect, useLocation } from "react-router-dom";
+import { setAuthedUser } from "../actions/authedUser";
 
-class SignIn extends React.Component {
-  state = {
-    value: 'sarahedo',
-  }
+function SignIn() {
+  const [authed, setAuthed] = useState("sarahedo");
+  const users = useSelector((state) => state.users);
+  const authedUser = useSelector((state) => state.authedUser);
+  const dispatch = useDispatch();
+  const location = useLocation();
 
-  handleChange = (e) => {
-    const value = e.target.value
-    this.setState(()=> ({
-      value,
-    }))
-  }
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setAuthed(value);
+  };
 
-  handleSubmit = (e) => {
-    const { dispatch } = this.props
-    const userId = this.state.value
-    
-    e.preventDefault()
+  const handleSubmit = (e) => {
+    const userId = authed;
+
+    e.preventDefault();
     if (userId) {
-      dispatch(setAuthedUser(userId))
-    } 
-  }
-
-  render() {
-    const { users, authedUser } = this.props
-    const { from } = this.props.location.state || { from: { pathname: '/' }}
-    
-    if (authedUser) {
-      return <Redirect push to={from} />
+      dispatch(setAuthedUser(userId));
     }
-    return (
-      <div className="sign-in-container mt-72">
-        <img 
-          src={require(`../images/login_illustration.png`)} 
-          alt="login illustration"/>
-        <div className="sign-in">
-          <h1>Sign in <br />to play</h1>
-          <form onSubmit={this.handleSubmit}>
-            <select 
-              value={this.state.value} 
-              onChange={this.handleChange}
-              className="mt-32 p-8"
-            >
-              {Object.keys(users).map((id) => (
-                <option key={id} value={id}>
-                  {users[id].name}
-                </option>
-              ))}
-            </select>
-            <button 
-              type='submit'
-              className="mt-16">
-              Sign In
-            </button>
-          </form>
-        </div>
+  };
+
+  const from = location.state || { from: { pathname: "/" } };
+
+  if (authedUser) {
+    return <Redirect push to={from} />;
+  }
+
+  return (
+    <div className="sign-in-container mt-72">
+      <img
+        src={require(`../images/login_illustration.png`)}
+        alt="login illustration"
+      />
+      <div className="sign-in">
+        <h1>
+          Sign in <br />
+          to play
+        </h1>
+        <form onSubmit={handleSubmit}>
+          <select value={authed} onChange={handleChange} className="mt-32 p-8">
+            {Object.keys(users).map((id) => (
+              <option key={id} value={id}>
+                {users[id].name}
+              </option>
+            ))}
+          </select>
+          <button type="submit" className="mt-16">
+            Sign In
+          </button>
+        </form>
       </div>
-    )
-  }
+    </div>
+  );
 }
 
-function mapStateToProps({ users, authedUser }) {
-  return {
-    users,
-    authedUser,
-  }
-}
-
-export default connect(mapStateToProps)(SignIn)
+export default SignIn;
